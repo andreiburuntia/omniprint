4a17a5d466bf93f8569ccae225d65843b152a954
Andrei Buruntia <andreiburuntia@gmail.com>
2018-01-10 02:34:31 +0200

    Add files via upload
    Added printer and job support

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, KeyboardAvoidingView, TextInput, Button, Alert, TouchableOpacity, ScrollView, ListView, TouchableHighlight, Modal } from 'react-native';
+import { Picker, View, Text, StyleSheet, KeyboardAvoidingView, TextInput, Button, Alert, TouchableOpacity, ScrollView, ListView, TouchableHighlight, Modal } from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 import * as firebase from 'firebase';
 
@@ -20,6 +20,9 @@ class JobsScreen extends React.Component {
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       text: '',
+      newJobName: '',
+      newJobPaperSize: '',
+      newJobColorOptions: '',
       modalVisible: false,
       dataSource: ds,
     };
@@ -46,7 +49,10 @@ class JobsScreen extends React.Component {
     itemsRef.on('value', (snap)=>{
       let items=[];
       snap.forEach((child) => {
-        items.push(child.val().title);
+        var inputDate = new Date(child.val().date);
+        var today = new Date();
+        if(inputDate.setHours(0,0,0,0)==today.setHours(0,0,0,0))
+          items.push({title: child.val().title, status: child.val().status, date: child.val().date});
       });
       console.log(items);
       this.setState({
@@ -59,7 +65,7 @@ class JobsScreen extends React.Component {
     return (
       <TouchableHighlight onPress={()=>this.pressRow(item)}>
         <View style={styles.li}>
-          <Text style={styles.liItem}>{item}</Text>
+          <Text style={item.status==='Finished' ? styles.finishedJob : item.status==="Failed" ? styles.failedJob : styles.inProgressJob}>{item.title} - {item.status}</Text>
           </View>
       </TouchableHighlight>
     )
@@ -86,23 +92,44 @@ class JobsScreen extends React.Component {
           visible={this.state.modalVisible}
           onRequestClose={() => {}}
           >
-          <View style={{marginTop: 22}}>
-            <View>
-              <Text>Hello world!</Text>
+          <View style={styles.modal}>
+            <View style={styles.modalInner}>
+
+              <Text style={styles.modalTitle}>Add a new print job</Text>
+
               <TextInput value={this.state.text}
-                placeholder="Enter text"
-                onChangeText={(value)=>this.setState({text:value})}/>
+                placeholder="Job name"
+                onChangeText={(value)=>this.setState({newJobName:value})}/>
+
+              <Picker
+                onValueChange={(value) => this.setState({newJobColorOptions: value})}>
+                <Picker.Item label="Monochrome" value="monochrome" />
+                <Picker.Item label="Color" value="color" />
+              </Picker>
+
+              <Picker
+                onValueChange={(value) => this.setState({newJobPaperSize: value})}>
+                <Picker.Item label="A4" value="a4" />
+                <Picker.Item label="Letter" value="letter" />
+              </Picker>
+
+              <View style={styles.modalBottomBuffer}/>
+
               <TouchableHighlight onPress={() => {
                 this.itemsRef.push({title: this.state.text})
                 this.setModalVisible(!this.state.modalVisible)                
-                }}>
-                <Text>Done</Text>
+                }}
+                style={styles.modalOkBtn}>
+                <Text style={styles.modalBtnText}>Done</Text>
               </TouchableHighlight>
+
               <TouchableHighlight onPress={() => {
                 this.setModalVisible(!this.state.modalVisible)                
-                }}>
-                <Text>Cancel</Text>
+                }}
+                style={styles.modalCancelBtn}>
+                <Text style={styles.modalBtnText}>Cancel</Text>
               </TouchableHighlight>
+
             </View>
           </View>
         </Modal>
@@ -119,9 +146,79 @@ class JobsScreen extends React.Component {
   }
   
   class PrintersScreen extends React.Component {
-    render() {
-      return <Text>List of all printers</Text>
-    }
+    constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds,
+    };
+
+    this.itemsRef = this.getRef().child('printers');
+
+    this.renderRow = this.renderRow.bind(this);
+    this.pressRow = this.pressRow.bind(this);
+  }
+
+  getRef(){
+    return firebaseApp.database().ref();
+  }
+
+  componentWillMount(){
+    this.getItems(this.itemsRef);
+  }
+
+  componentDidMount(){
+    this.getItems(this.itemsRef);
+  }
+
+  getItems(itemsRef){
+    itemsRef.on('value', (snap)=>{
+      let items=[];
+      snap.forEach((child) => {
+          items.push({title: child.val().title, details: child.val().details, status: child.val().status});
+      });
+      console.log(items);
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      })
+    });    
+  }
+
+  renderRow(item){
+    return (
+      <TouchableHighlight onPress={()=>this.pressRow(item)}>
+        <View style={styles.li}>
+          <Text style={item.status==='Online' ? styles.finishedJob : item.status==="Busy" ? styles.failedJob : styles.inProgressJob}>{item.title} - {item.status}</Text>
+          </View>
+      </TouchableHighlight>
+    )
+  }
+
+  pressRow(item){
+    console.log("PRESSED: "+item);
+  }
+
+  setModalVisible(visible){
+    this.setState({modalVisible:visible});
+  }
+
+  addItem(){
+    this.setModalVisible(true);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
+        <TouchableOpacity style={styles.addBtn} onPress={this.addItem.bind(this)}>
+          <Text style={styles.addBtnText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   }
   
   const MainScreenNavigator = TabNavigator({
@@ -140,8 +237,80 @@ class JobsScreen extends React.Component {
     container: {
       flex: 1,
     },
-    scrollContainer: {
-
+    modal: {
+      marginTop: 22,
+      flex: 1,
+    },
+    modalInner:{
+      marginLeft: 20,
+      marginRight: 20,      
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+    modalTitle:{
+      fontSize: 30,
+      textAlign: 'center',
+      fontWeight: 'bold'
+    },
+    modalBottomBuffer:{
+      height: 80,
+    },
+    modalCancelBtn: {
+      backgroundColor: '#e57373',
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      width: 120,
+      height: 60,
+      borderRadius: 80,
+      marginRight: 10,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,
+    },
+    modalOkBtn:{
+      backgroundColor: '#4DB6AC',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      width: 120,
+      height: 50,
+      borderRadius: 80,
+      marginLeft: 10,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,
+    },
+    modalBtnText: {
+      fontSize: 20,
+      textAlign: 'center'
+    },
+    finishedJob: {
+      backgroundColor: '#A5D6A7',
+      fontSize: 30,
+      paddingLeft: 16,
+      paddingRight: 16,
+      paddingTop: 14,
+      paddingBottom:16
+    },
+    failedJob: {
+      backgroundColor: '#ef9a9a',
+      fontSize: 30,
+      paddingLeft: 16,
+      paddingRight: 16,
+      paddingTop: 14,
+      paddingBottom:16
+    },
+    inProgressJob: {
+      backgroundColor: '#81D4FA',
+      fontSize: 30,
+      paddingLeft: 16,
+      paddingRight: 16,
+      paddingTop: 14,
+      paddingBottom:16
     },
     addBtn: {
       position: 'absolute',
@@ -164,9 +333,6 @@ class JobsScreen extends React.Component {
       borderBottomColor: '#eee',
       borderColor: 'transparent',
       borderWidth: 1,
-      paddingLeft: 16,
-      paddingTop: 14,
-      paddingBottom:16
     }
   });
 
